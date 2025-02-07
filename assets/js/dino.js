08b038786a9587c43fe7a97ae0ad368ccd97d4ff
Edvin Listo Zec <edvin.l.zec@gmail.com>
2025-02-07 14:06:54 +0100

    Update dino.js

--- assets/js/dino.js
+++ assets/js/dino.js
@@ -1,4 +1,4 @@
-const canvas = document.createElement('canvas');
+const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 const size = 400;
 canvas.width = size;
